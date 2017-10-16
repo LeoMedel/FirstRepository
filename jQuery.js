ed1f7cd733bf296a208335e7a3512a072460bcb0
LeoMedel <leonel.ilhuicatzi07@gmail.com>
2017-10-16 05:37:00 -0500

    New Update Star Wars et jQuery

--- jQuery.js
+++ jQuery.js
@@ -2,13 +2,17 @@
 $(document).ready(function(){
 
         $.get( "https://swapi.co/api/people/?format=json", function( data ) {
-            $( ".result" ).html( data.name);
+            
 
             for (var i = 0; i < 6; i++)
             {
+            	$( ".result"+i+"").html( data.results[i].name);
             	console.log('Personage '+(i+1)+': '+ data.results[i].name);
             }
             
-
         });
        });
+
+
+
+
