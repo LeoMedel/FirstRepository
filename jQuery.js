b5faad777e157567926a54f9a881f462afe0d72b
LeoMedel <leonel.ilhuicatzi07@gmail.com>
2017-10-23 15:25:43 -0500

    Changes in Star Wars Page and jQuery

--- jQuery.js
+++ jQuery.js
@@ -1,24 +1,38 @@
 
 
-var nPer = 1;
-var nFilms = 4;
+function myfunction()
+      {
+        console.log("survol");
+      }
 
-$.ajax({
-	url: "https://swapi.co/api/people/?format=json",
-	type: 'GET',
-	dataType: 'json',
-	success: function(personnage)
-	{
-		for (var i = 0; i < nPer; i++)
-        {
+function appelAlt(per)
+{
+	iman = document.images[per].alt;
+	console.log(iman);
+}
 
-        	$("#but").attr("type", personnage.next);
-        	
-        	$(".result"+i).append("<h4 class = 'nom"+i+"'></h4>");
-            $( ".nom"+i).html( personnage.results[i].name);
-            $(".list-group").append("<button type='button' class='list-group-item list-group-item-action', data-toggle='modal', data-target='#myModal'>"+personnage.results[i].name+"</button>");
-            
-            $(".modal-body").append("<ul class='list-group'>"+
+
+var appiSW = "https://swapi.co/api/people/?format=json";
+
+function cargaPersonajes()
+{
+	$.ajax({
+		url: "https://swapi.co/api/people/?format=json",
+		type: 'GET',
+		dataType: 'json',
+		success: function(personnage)
+		{
+			$( ".list-group" ).empty();
+			$( ".nom" ).empty();
+			for (var i = 0; i < personnage.results.length; i++)
+			{
+				$(".result"+i).append("<h4 class = 'nom"+i+"'></h4>");
+				$( ".nom"+i).html( personnage.results[i].name);
+
+            	$(".list-group").append("<button type='button' class='list-group-item list-group-item-action', data-toggle='modal', data-target='#myModal'>"+personnage.results[i].name+"</button>");
+
+            	$( ".modal-body" ).empty();
+            				$(".modal-body").append("<ul class='list-group'>"+
 									  "<li class='list-group-item active'>Name</li>"+
 									  "<li class='list-group-item'>"+personnage.results[i].name+"</li>"+
 									  "<li class='list-group-item active'>Mass</li>"+
@@ -28,49 +42,31 @@ $.ajax({
 									  "<li class='list-group-item active'>Films</li>"+
 									  "<li class='list-group-item film'></li>"+
 									"</ul>");
-            
 
+	            console.log("    PERSONAJE:   " + i +" "+ personnage.results[i].name);
 
-            console.log('Personage '+i+': '+ personnage.results[i].name);
+	            for(var e = 0; e< personnage.results[i].films.length; e++)
+	            {
+	            	console.log("   Hola:          "+e +"   "+personnage.results[i].name);
+	            	console.log("   PELI:          "+e +"   "+personnage.results[i].films);
+	            	console.log("   PELICULA:          "+e +"   "+personnage.results[i].films[e]);
+	            	console.log("   Nombre PELICULA:          "+e +"   "+personnage.results[i].films[e].title);
+	            	$.ajax({
+						url: personnage.results[i].films[e],
+						type: 'GET',
+						dataType: 'json',
+						success: function(peli)
+						{
+							$( ".film").empty();
+							$(".film").append(
+								"<p class='films'>"+peli.title+"</p>");
+						}
+					})
+					
+	            }	
+			}
 
-
-
-            for(var e = 0; e< nFilms; e++)
-            {
-            	$.ajax({
-					url: personnage.results[i].films[e],
-					type: 'GET',
-					dataType: 'json',
-					success: function(films)
-					{
-						$(".film").append(
-							"<p class='films'>"+films.title+"</p>");
-
-						console.log('title du film '+films.title);
-
-					}
-				})
-            }
-        }
-	}
-})
-
-
-
-
-
-
-
-
-
-
-
-
-
-            
-
-
-
-
-
-
+		}
+	});
+	
+}
