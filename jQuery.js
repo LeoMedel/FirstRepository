ee49007304c3ff90394d67f34a842388da071a64
LeoMedel <leonel.ilhuicatzi07@gmail.com>
2017-11-01 18:03:53 -0600

    Update in jQuery for personnages details

--- jQuery.js
+++ jQuery.js
@@ -13,16 +13,12 @@ function appelAlt(per)
 
 function creeModal(nom, mass, eyes, he, hair, skin, birth, gender, personnage)
 {
-	console.log("numero de peliculas: "+personnage.length);
-
-
+	
 	$( ".modal-body" ).empty();
 	$(".modal-body").append("<ul class='list-group'>"+
-								
 									"<li class='list-group-item'>"+
 										"<h1 class = 'nom'>"+nom+"</h1>"+
 									"</li>"+
-
 									"<li class='list-group-item'>Masse: "+mass+"</li>"+
 									"<li class='list-group-item'>Couleur des yeux: "+eyes+"</li>"+
 									"<li class='list-group-item'>Taille: "+he+"</li>"+
@@ -30,24 +26,21 @@ function creeModal(nom, mass, eyes, he, hair, skin, birth, gender, personnage)
 									"<li class='list-group-item'>Peau: "+skin+"</li>"+
 									"<li class='list-group-item'>Joyeux anniversaire: "+birth+"</li>"+
 									"<li class='list-group-item'>Genre: "+gender+"</li>"+
-
-									
 								"<li class='list-group-item active'>Films</li>"+
 								"<li class='list-group-item film'>Loading...</li>"+
 							"</ul>");
+	
 	$.ajax({
 		url: personnage,
 		type: 'GET',
 		dataType: 'json',
 		success: function(peli)
 		{
-
 			console.log("pelicula: "+peli.title);
 			$( ".film" ).empty();
 			$(".film").append("<p class='film'>"+peli.title+"</p>");
 		}
 	})
-	
 }
 
 
@@ -63,6 +56,7 @@ function cargaPersonajes(pag)
 			dataType: 'json',
 			success: function(personnage)
 			{
+				console.log("Que es personnage???: " + personnage);
 				console.log("pagina anterior "+personnage.previous);
 				$( ".list-group" ).empty();
 				$( ".nom" ).empty();
@@ -73,6 +67,13 @@ function cargaPersonajes(pag)
 
 				for (var i = 0; i < personnage.results.length; i++)
 				{
+					//Aouter nom aux Images
+					if(pag === "https://swapi.co/api/people/?format=json")
+					{
+						$(".result"+i).append("<h4 class = 'nom"+i+"'></h4>");
+						$( ".nom"+i).html( personnage.results[i].name);
+					}
+
 					console.log("Personaje "+i+" "+personnage.results[i].name);
 					
 					var nomPer = personnage.results[i].name;
@@ -83,51 +84,28 @@ function cargaPersonajes(pag)
 					var skin = personnage.results[i].skin_color;
 					var birth = personnage.results[i].birth_year;
 					var gen = personnage.results[i].gender;
-					var fil = personnage.results[i].films;
+					var fl = personnage.results[i].films;
+					console.log("Que tiene fl: "+fl[0]);
+					var numPel = personnage.results[i].films.length;
 					
-					//AJOUTER Nom aux images
-					if(pag === "https://swapi.co/api/people/?format=json")
-					{
-						$(".result"+i).append("<h4 class = 'nom"+i+"'></h4>");
-						$( ".nom"+i).html( personnage.results[i].name);
-					}
-					
-					var pel = personnage.results[i].films+" "; 
-					//console.log("Que resulto de PELICULAS: "+personnage.results[i].films);
-					
-					//onclick = \"creeModal('"+nomPer+"', '"+mass+"', '"+eye+"', '"+hei+"', '"+hair+"', '"+skin+"', '"+birth+"', '"+gen+"', '"+personnage.results[i].films[e]+"')\",
-					
+					console.log("Mega URL: "+personnage.results[i].films);
+					console.log("Mega URL 1111: "+personnage.results[i].films[0]);
+		            console.log("LONGITUD DE LA MEGA URL: " + personnage.results[i].films.length);
+
+
 	            	for(var e = 0; e< personnage.results[i].films.length; e++)
 		            {
-		            	console.log("Mega URL: "+personnage.results[i].films);
-		            	console.log("Que es esto? "+personnage.results[i].films[e]);
-		            	$(".list-group").append("<button type='button', onclick = \"creeModal('"+nomPer+"', '"+mass+"', '"+eye+"', '"+hei+"', '"+hair+"', '"+skin+"', '"+birth+"', '"+gen+"', '"+personnage.results[i].films[e]+"')\","+
-	            	 						"class='list-group-item list-group-item-action perso', data-toggle='modal', data-target='#myModal'>"+personnage.results[i].name+"</button>");
-		            	//$(".perso").attr("onclick", "creeModal('"+nomPer+"', '"+mass+"', '"+eye+"', '"+hei+"', '"+hair+"', '"+skin+"', '"+birth+"', '"+gen+"', '"+personnage.results[i].films[e]+"')");
-	            	
-		            	/*
-		            	$.ajax({
-							url: personnage.results[i].films[e],
-							type: 'GET',
-							dataType: 'json',
-							success: function(peli)
-							{
+		            	$(".list-group").append("<button type='button', onclick = \"creeModal('"+nomPer+"', '"+mass+"', '"+eye+"', '"+hei+"', '"+hair+"', '"+skin+"', '"+birth+"', '"+gen+"', '"+fl[e]+"')\","+
+		            							"class='list-group-item list-group-item-action perso', data-toggle='modal', data-target='#myModal'>"+personnage.results[i].name+"</button>");
 
-								console.log(": "+peli.title);
-								//$(".film").append("<p class='film'>"+peli.title+"</p>");
-							}
-						})
-						*/
 		            }
 				}
-
 			}
 		});
 
 	}
 	else if (pag === "null")
 	{
-		console.log("Ya son todas las paginas");
-
+		alert("Il n'y a plus de page de personnages de Star Wars");
 	}
 }
